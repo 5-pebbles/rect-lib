0b89e8da1a897577c6b37c7a36a599c3afffa8d4
5-pebbles <5-pebble@protonmail.com>
2024-04-21 15:09:16 +0000

    docs(trait): add docs & doc tests to Rectangle trait;

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,49 @@ pub use num;
 mod basic_rectangle;
 pub use basic_rectangle::BasicRectangle;
 
-// & here's our trait
+/// A trait containing methods for rectangle like data structures which implement `Sized` & `Copy`.
+///
+/// # Example
+/// ```
+/// use rect_lib::Rectangle;
+///
+/// #[derive(Clone, Copy)]
+/// pub struct BasicRectangle {
+///     x: i32,
+///     y: i32,
+///     width: i32,
+///     height: i32,
+/// }
+///
+/// impl Rectangle for BasicRectangle {
+///     type Unit = i32;
+///
+///     fn left(&self) -> i32 {
+///         self.x
+///     }
+///
+///     fn right(&self) -> i32 {
+///         self.x + self.width - 1
+///     }
+///
+///     fn top(&self) -> i32 {
+///         self.y
+///     }
+///
+///     fn bottom(&self) -> i32 {
+///         self.y - self.height + 1
+///     }
+///
+///     fn new_from_sides(left: i32, right: i32, top: i32, bottom: i32) -> Self {
+///         BasicRectangle {
+///             x: left,
+///             y: top,
+///             width: right - left + 1,
+///             height: top - bottom + 1,
+///         }
+///     }
+/// }
+/// ```
 pub trait Rectangle
 where
     Self: Sized + Copy,
